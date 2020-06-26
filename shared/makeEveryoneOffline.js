13258926c01be60a0bde60773aaa1759d8c47029
Jonathan Dilks <jonathan@vouch.co.uk>
2020-06-26 13:14:45 +0100

    No need to specify TWILIO_WORKSPACE_SID anymore

--- shared/makeEveryoneOffline.js
+++ shared/makeEveryoneOffline.js
@@ -1,20 +1,25 @@
-const accountSid =  process.env.TWILIO_ACCOUNT_SID;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const workSpaceSid = process.env.TWILIO_WORKSPACE_SID;
+//const workSpaceSid = process.env.TWILIO_WORKSPACE_SID;
 const offlineActivitySid = process.env.TWILIO_OFFLINE_ACTIVITY_SID;
 const client = require('twilio')(accountSid, authToken);
 
 module.exports = async function () {
+    // Work out the workspace SID (only supports one workspace presently, foreach for more)
+    const allWorkspaces = await client.taskrouter.workspaces.list();
+    const workSpaceSid = allWorkspaces[0].sid;
 
-    const allNotOfflineWorkers = (await client.taskrouter.workspaces(workSpaceSid)
-        .workers
-        .list())
+    //Grab the workers from the workspace
+    const taskRouterForWorkspace = client.taskrouter.workspaces(workSpaceSid);
+    const allWorkers = await taskRouterForWorkspace.workers.list()
+    const allWorkersNotOffline = allWorkers
         .filter(x => x.activitySid != offlineActivitySid);
 
-    console.log(`${allNotOfflineWorkers.length} workers not Offline`);
+    console.log(`${allWorkersNotOffline.length}/${allWorkers.length} workers not Offline`);
 
-    allNotOfflineWorkers.forEach(worker => {
-        worker.update({ activitySid: offlineActivitySid })
+    //Do it (make them offline)
+    allWorkersNotOffline.forEach(worker => {
+        //await worker.update({ activitySid: offlineActivitySid })
         console.log(`${worker.friendlyName} was ${worker.activityName}, should now be Offline`);
     });
 
